6f46025687eafc7d09380f63da4bffc9baed4fa4
Unknown <slyguggi@gmail.com>
2019-02-06 08:37:55 +0100

    fixed various bugs related to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//todo player 180° by double tap in same frame then again is causing problem (maybe?)
+//todo rework boxcast to be more precise
+
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float fallMultiplier = 2.5f;
@@ -10,7 +13,7 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float playerHorizontalSpeed = 5.0f;
 	[SerializeField] private float gravityMultiplier = 1.0f;
 	[SerializeField] private float jumpSpeed = 3.0f;
-	[SerializeField] private float rotationSpeed = 5.0f;
+	[SerializeField] private float rotationTime = 1.75f;
 	[SerializeField] private float distMaxGroundCheck = 0.1f;
 	[SerializeField] private Vector2 sizeMaxCheckGround = new Vector2(0.5f, 0.1f);
 	[SerializeField] private LayerMask layerGround = 0;
@@ -78,14 +81,15 @@ public class PlayerController : MonoBehaviour
 	{
 		_horizontalInput = Input.GetAxis("Horizontal");
 
-		bool isGroundedPastValue = _isGrounded;
+		CheckGrounded();
+		/*bool isGroundedPastValue = _isGrounded;
 
 		_isGrounded = Physics2D.BoxCast(transform.position, sizeMaxCheckGround, 0, -transform.up, distMaxGroundCheck,
 			layerGround);
 		if (_isGrounded && !isGroundedPastValue)
 		{
 			_numberGravityUseRemaining = maxNumberGravityUse;
-		}
+		}*/
 
 		if (Input.GetButtonDown("Jump") && _isGrounded)
 		{
@@ -97,8 +101,8 @@ public class PlayerController : MonoBehaviour
 		}
 
 		if ((Input.GetButtonDown("TurnLeft") && Input.GetButtonDown("TurnRight") && _numberGravityUseRemaining > 0) ||
-		    (_isPressingLeft && Input.GetButtonDown("TurnRight")) ||
-		    (_isPressingRight && Input.GetButtonDown("TurnLeft")))
+			(_isPressingLeft && Input.GetButtonDown("TurnRight")) ||
+			(_isPressingRight && Input.GetButtonDown("TurnLeft")))
 		{
 			if (!_isPressingLeft && !_isPressingRight)
 			{
@@ -138,27 +142,42 @@ public class PlayerController : MonoBehaviour
 		{
 			_myRigidbody.AddForce(transform.up.normalized * gravityMultiplier * Physics2D.gravity.y);
 			_myRigidbody.velocity = transform.right.normalized * playerHorizontalSpeed * _horizontalInput +
-			                        Vector3.Project(_myRigidbody.velocity, transform.up);
+									Vector3.Project(_myRigidbody.velocity, transform.up);
 			if (_isPressingJump)
 			{
 				_myRigidbody.velocity = transform.up.normalized * jumpSpeed +
-				                        Vector3.Project(_myRigidbody.velocity, transform.right);
+										Vector3.Project(_myRigidbody.velocity, transform.right);
 				_isPressingJump = false;
 			}
 
 			if (Vector3.Dot(Vector3.Project(_myRigidbody.velocity, transform.up).normalized,
-				    transform.up.normalized) < 0)
+					transform.up.normalized) < 0)
 			{
 				_myRigidbody.velocity += (Vector2) transform.up.normalized * Physics2D.gravity.y *
-				                         (fallMultiplier - 1) * Time.deltaTime;
+										 (fallMultiplier - 1) * Time.deltaTime;
 			}
 			else if (Vector3.Dot(Vector3.Project(_myRigidbody.velocity, transform.up).normalized,
-				         transform.up.normalized) > 0 && !Input.GetButton("Jump"))
+						 transform.up.normalized) > 0 && !Input.GetButton("Jump"))
 			{
 				_myRigidbody.velocity +=
 					(Vector2) transform.up.normalized * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 			}
 		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (_isGrounded)
+		{
+			_numberGravityUseRemaining = maxNumberGravityUse;
+		}
+	}
+
+	private void CheckGrounded()
+	{
+		Debug.Log(transform.up.ToString());
+		_isGrounded = Physics2D.BoxCast(transform.position, sizeMaxCheckGround, 0,
+			-transform.up, distMaxGroundCheck, layerGround);
 	}
 
 	private IEnumerator ResetPressingTurn(float timeToWait)
@@ -180,21 +199,21 @@ public class PlayerController : MonoBehaviour
 			StopCoroutine(_rotatingCoroutine);
 		}
 
-		_rotatingCoroutine = StartCoroutine(TurnCameraAndPlayer(rotationSpeed));
+		_rotatingCoroutine = StartCoroutine(TurnCameraAndPlayer(rotationTime));
 	}
 
-	private IEnumerator TurnCameraAndPlayer(float speedTurn)
+	private IEnumerator TurnCameraAndPlayer(float time)
 	{
-		float time = (90.0f / speedTurn);
 		float timer = 0.0f;
 		float initRotPlayer = transform.eulerAngles.z;
 		while (timer < time)
 		{
-			float angle = (float) _actualGravityDirection * 90;
-			Vector3 playerRotation = transform.eulerAngles;
-			transform.eulerAngles = (Vector3.right * playerRotation.x + Vector3.up * playerRotation.y +
-			                         Vector3.forward * (Mathf.LerpAngle(initRotPlayer, angle, timer / time)));
 			timer += Time.deltaTime;
+			float angle = (float) _actualGravityDirection * 90.0f;
+			//Vector3 playerRotation = transform.eulerAngles;
+			_myRigidbody.rotation = (Mathf.LerpAngle(initRotPlayer, angle, timer / time));
+			//transform.eulerAngles = (Vector3.right * playerRotation.x + Vector3.up * playerRotation.y +
+			//						 Vector3.forward * (Mathf.LerpAngle(initRotPlayer, angle, timer / time)));
 			yield return null;
 		}
 
