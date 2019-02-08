fa6552bfae23e9e9fedefef7a71f45cac9c7e1ae
Unknown <slyguggi@gmail.com>
2019-02-08 14:03:36 +0100

    added laser turning related to player gravity

--- Assets/Scripts/LaserBehaviour.cs
+++ Assets/Scripts/LaserBehaviour.cs
@@ -7,31 +7,53 @@ public class LaserBehaviour : MonoBehaviour
 	[SerializeField] private float warmUpTime = 0.5f;
 	[SerializeField] private float activeTime = 2.0f;
 	[SerializeField] private float inactiveTime = 2.0f;
+	[SerializeField] private LayerMask layerGround = 0;
+	[SerializeField] private float distance = 100.0f;
 	private bool _isActive = true;
-	private SpriteRenderer _mySprite = null;
+	private SpriteRenderer _mySprite;
+	private LineRenderer _myLineRenderer;
+	private BoxCollider2D _myCollider;
 
 	private void Start()
 	{
-		//_mySprite = GetComponentInChildren<SpriteRenderer>();
-		//StartCoroutine(SimpleRoutine());
+		_myLineRenderer = GetComponent<LineRenderer>();
+		_myCollider = GetComponentInChildren<BoxCollider2D>();
+		_mySprite = GetComponentInChildren<SpriteRenderer>();
+		StartCoroutine(SimpleRoutine());
+	}
+
+	private void Update()
+	{
+		//follows player rotation
+		Vector2 currentPos = transform.position;
+		RaycastHit2D hit = Physics2D.Raycast(currentPos, -transform.up, distance,
+			layerGround);
+		transform.rotation = GameManager.Instance.Player.transform.rotation;
+		_myLineRenderer.SetPosition(0, currentPos);
+		_myLineRenderer.SetPosition(1, hit.point);
+		_myCollider.offset =Vector2.down * (hit.point - currentPos).magnitude / 2;
+		_myCollider.size = Vector2.right * _myCollider.size + Vector2.up * (hit.point - currentPos).magnitude;
 	}
 
 	private IEnumerator SimpleRoutine()
 	{
 		while (true)
 		{
-			//todo change _mySprite to warmingup
-			_mySprite.color=Color.grey;
+			ChangeLineColor(Color.gray);
 			yield return new WaitForSeconds(warmUpTime);
-			//todo change _mySprite to active
-			_mySprite.color = Color.white;
+			ChangeLineColor(Color.white);
 			_isActive = true;
 			yield return new WaitForSeconds(activeTime);
-			//todo change _mySprite to inactive
-			_mySprite.color = Color.clear;
+			ChangeLineColor(Color.clear);
 			_isActive = false;
 			yield return new WaitForSeconds(inactiveTime);
 		}
+	}
+
+	private void ChangeLineColor(Color color)
+	{
+		_myLineRenderer.startColor = color;
+		_myLineRenderer.endColor = color;
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
