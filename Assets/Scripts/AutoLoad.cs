597846b5908a92f7e47f70e06b3de1a00d482ffd
Unknown <slyguggi@gmail.com>
2019-02-28 19:47:29 +0100

    upload from laptop

---
+++ Assets/Scripts/AutoLoad.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLoad : MonoBehaviour
+{
+	private void Start()
+	{
+		GameManager.Instance.LoadLevel("MainMenu");
+	}
+}
